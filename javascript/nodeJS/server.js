29753df7cac06414feba3ac69d011f12f5fa1242
npurple <mz1985@163.com>
2015-10-30 16:15:40 +0800

    add index.js, use server as a export model

--- javascript/nodeJS/server.js
+++ javascript/nodeJS/server.js
@@ -1,12 +1,16 @@
 var http = require('http');
 var port = 8124;
 
-function onRequest(request, response) {
-    console.log('Request received.');
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World');
-    response.end();
+function start() {
+    function onRequest(request, response) {
+        console.log('Request received.');
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write('Hello World');
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(port);
+    console.log('Server has started on port: ' + port);
 }
 
-http.createServer(onRequest).listen(port);
-console.log('Server has started on port: ' + port);
+exports.start = start;
