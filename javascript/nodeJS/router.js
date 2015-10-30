e7f94d05741fbfef4d7a3b324ab00baff699054e
npurple <mz1985@163.com>
2015-10-30 16:50:34 +0800

    add handler module to match the router

--- javascript/nodeJS/router.js
+++ javascript/nodeJS/router.js
@@ -1,5 +1,10 @@
-function route(path) {
+function route(handle, path) {
     console.log('Route to path: ' + path);
+    if (typeof handle[path] === 'function') {
+        handle[path]();
+    } else {
+        console.log('No request handler found for path: ' + path);
+    }
 }
 
 exports.route = route;
--- javascript/nodeJS/server.js
+++ javascript/nodeJS/server.js
@@ -2,12 +2,12 @@ var http = require('http');
 var url = require('url');
 var port = 8124;
 
-function start(route) {
+function start(route, handle) {
     function onRequest(request, response) {
         var path = url.parse(request.url).pathname;
         console.log('Request path: ' + path);
 
-        route(path);
+        route(handle, path);
 
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write('Hello World');
